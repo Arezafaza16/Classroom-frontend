a7545c3b7c552413ceebd0cd468107f9871631ca
Arezafaza16 <arezafaza@gmail.com>
2026-03-11 09:39:17 +0700

    setup frontend

--- src/constant/mockData.ts
+++ src/constant/mockData.ts
@@ -16,7 +16,7 @@ export const mockSubjects: Subject[] = [
         name: "Linear Algebra",
         department: "Mathematics",
         description:
-            "An intermediate course exploring vector spaces, matrix operations, eigenvalues, and their applications in science and engineering.",
+            "An intermediate course exploring vector spaces, matrix operations, eigenvalues, and their applications in science and engineering is very good.",
         createdAt: new Date().toISOString(),
     },
     {
